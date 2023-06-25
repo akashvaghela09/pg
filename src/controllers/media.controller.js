e8abc31f924f0a7e82f1045f98677ca72f8d2346
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-06-25 18:45:00 +0530

    Update text cleaning

--- src/controllers/media.controller.js
+++ src/controllers/media.controller.js
@@ -4,6 +4,7 @@ const axios = require("axios");
 const { config } = require("../configs/config");
 const { domain } = config;
 const fs = require("fs");
+const { cleanTitlesPG } = require("../utils/filter");
 
 const getTitleDetails = async (req, res) => {
     // const { type = "" } = req.query;
@@ -30,138 +31,27 @@ const getTitleDetails = async (req, res) => {
 
         // Scrap Nudiity Section
         let nuditySection = page.querySelector("#advisory-nudity");
-        let nudity = {};
-        let nudityTitle = nuditySection.querySelector("h4.ipl-list-title").textContent;
-        let nudityLevel = nuditySection.querySelector("span.ipl-status-pill").textContent;
-        let nudityVotes = nuditySection.querySelector("a.advisory-severity-vote__message").textContent;
-
-        nudity.title = nudityTitle;
-        nudity.level = nudityLevel;
-        nudity.votes = nudityVotes;
-        nudity.list = [];
-
-        // Get the <ul> element by its class name
-        let nudityUlTag = nuditySection.querySelector("ul.ipl-zebra-list");
-
-        // Get all <li> elements within the <ul> element
-        let nudityLiTags = nudityUlTag.querySelectorAll("li.ipl-zebra-list__item");
-
-        // Loop through each <li> and extract the text
-        for (let i = 0; i < nudityLiTags.length; i++) {
-            let text = nudityLiTags[i].textContent.trim(); // Trim to remove leading/trailing whitespace
-            let cleanText = text.split("Edit").join("");
-            nudity.list.push(cleanText.trim());
-        }
+        let nudity = cleanTitlesPG(nuditySection);
         parentalGuideData.push(nudity);
 
         // Scrap Violence Section
         let violenceSection = page.querySelector("#advisory-violence");
-        let violence = {};
-        let violenceTitle = violenceSection.querySelector("h4.ipl-list-title").textContent;
-        let violenceLevel = violenceSection.querySelector("span.ipl-status-pill").textContent;
-        let violenceVotes = violenceSection.querySelector("a.advisory-severity-vote__message").textContent;
-
-        violence.title = violenceTitle;
-        violence.level = violenceLevel;
-        violence.votes = violenceVotes;
-        violence.list = [];
-
-        // Get the <ul> element by its class name
-        let violenceUlTag = violenceSection.querySelector("ul.ipl-zebra-list");
-
-        // Get all <li> elements within the <ul> element
-        let violenceLiTags = violenceUlTag.querySelectorAll("li.ipl-zebra-list__item");
-
-        // Loop through each <li> and extract the text
-        for (let i = 0; i < violenceLiTags.length; i++) {
-            let text = violenceLiTags[i].textContent.trim(); // Trim to remove leading/trailing whitespace
-            let cleanText = text.split("Edit").join("");
-            violence.list.push(cleanText.trim());
-        }
-
+        let violence = cleanTitlesPG(violenceSection);
         parentalGuideData.push(violence);
 
         // Scrap Profanity Section
         let profanitySection = page.querySelector("#advisory-profanity");
-        let profanity = {};
-        let profanityTitle = profanitySection.querySelector("h4.ipl-list-title").textContent;
-        let profanityLevel = profanitySection.querySelector("span.ipl-status-pill").textContent;
-        let profanityVotes = profanitySection.querySelector("a.advisory-severity-vote__message").textContent;
-
-        profanity.title = profanityTitle;
-        profanity.level = profanityLevel;
-        profanity.votes = profanityVotes;
-        profanity.list = [];
-
-        // Get the <ul> element by its class name
-        let profanityUlTag = profanitySection.querySelector("ul.ipl-zebra-list");
-
-        // Get all <li> elements within the <ul> element
-        let profanityLiTags = profanityUlTag.querySelectorAll("li.ipl-zebra-list__item");
-
-        // Loop through each <li> and extract the text
-        for (let i = 0; i < profanityLiTags.length; i++) {
-            let text = profanityLiTags[i].textContent.trim(); // Trim to remove leading/trailing whitespace
-            let cleanText = text.split("Edit").join("");
-            profanity.list.push(cleanText.trim());
-        }
-
+        let profanity = cleanTitlesPG(profanitySection);
         parentalGuideData.push(profanity);
 
         // Scrap Alcohol Section
         let alcoholSection = page.querySelector("#advisory-alcohol");
-        let alcohol = {};
-        let alcoholTitle = alcoholSection.querySelector("h4.ipl-list-title").textContent;
-        let alcoholLevel = alcoholSection.querySelector("span.ipl-status-pill").textContent;
-        let alcoholVotes = alcoholSection.querySelector("a.advisory-severity-vote__message").textContent;
-
-        alcohol.title = alcoholTitle;
-        alcohol.level = alcoholLevel;
-        alcohol.votes = alcoholVotes;
-        alcohol.list = [];
-
-        // Get the <ul> element by its class name
-        let alcoholUlTag = alcoholSection.querySelector("ul.ipl-zebra-list");
-
-        // Get all <li> elements within the <ul> element
-        let alcoholLiTags = alcoholUlTag.querySelectorAll("li.ipl-zebra-list__item");
-
-        // Loop through each <li> and extract the text
-
-        for (let i = 0; i < alcoholLiTags.length; i++) {
-            let text = alcoholLiTags[i].textContent.trim(); // Trim to remove leading/trailing whitespace
-            let cleanText = text.split("Edit").join("");
-            alcohol.list.push(cleanText.trim());
-        }
-
+        let alcohol = cleanTitlesPG(alcoholSection);
         parentalGuideData.push(alcohol);
 
         // Scrap Frightening Section
         let frighteningSection = page.querySelector("#advisory-frightening");
-        let frightening = {};
-
-        let frighteningTitle = frighteningSection.querySelector("h4.ipl-list-title").textContent;
-        let frighteningLevel = frighteningSection.querySelector("span.ipl-status-pill").textContent;
-        let frighteningVotes = frighteningSection.querySelector("a.advisory-severity-vote__message").textContent;
-
-        frightening.title = frighteningTitle;
-        frightening.level = frighteningLevel;
-        frightening.votes = frighteningVotes;
-        frightening.list = [];
-
-        // Get the <ul> element by its class name
-        let frighteningUlTag = frighteningSection.querySelector("ul.ipl-zebra-list");
-
-        // Get all <li> elements within the <ul> element
-        let frighteningLiTags = frighteningUlTag.querySelectorAll("li.ipl-zebra-list__item");
-
-        // Loop through each <li> and extract the text
-        for (let i = 0; i < frighteningLiTags.length; i++) {
-            let text = frighteningLiTags[i].textContent.trim(); // Trim to remove leading/trailing whitespace
-            let cleanText = text.split("Edit").join("");
-            frightening.list.push(cleanText.trim());
-        }
-
+        let frightening = cleanTitlesPG(frighteningSection);
         parentalGuideData.push(frightening);
 
         returnResponse.parentalGuide = parentalGuideData;
